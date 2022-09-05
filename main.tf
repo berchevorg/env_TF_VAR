13207077e57f68e5cdbd3223f9f545302bf004ce
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-09-05 09:25:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 variable "length" {}
+variable "name" {}
 
 resource "random_pet" "name" {
  length    = var.length
  separator = "-"
 }
+
+provisioner "local-exec" {
+command = <<EOT
+echo $TF_VAR_name
+EOT
